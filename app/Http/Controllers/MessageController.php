0ba60d43c7fc88c6b6b168a74381e3f649e5f502
liuchengguo <liuchengguo@we-te.com>
2017-01-03 10:38:17 +0800

    no message

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Guo\Wechat\Http\Controllers;
+
+
+use Guo\Wechat\Model\Message;
+use App\User;
+use Illuminate\Http\Request;
+
+/**
+ * Class WechatController
+ * @package App\Http\Controllers
+ *
+ * 规则中有一些特殊规则的id限定死,安排如下
+ * 1,关注回复
+ * 2.默认回复(暂时未启用)
+ * 3.投票口令 跟随投票口令一起修改,用户输入"投票口令",公众号返回具体的投票口令内容
+ * 4.实际的投票口令内容
+ */
+class MessageController extends CommonController
+{
+
+    public function index(Request $request)
+    {
+        $where = array();
+        $like = array();
+        $msg_type = $request->msg_type;
+        $content = $request->content;
+        $today = isset($request->today)?$request->today:'今天';
+        $yestoday = $request->yesterday;
+        $day= $request->day;
+        $table_type = '';
+        if ($msg_type !== 0 && !empty($msg_type)) {
+            $where['msg_type'] = $msg_type;
+        }
+        if (!empty($content)) {
+            $where['content'] = $content;
+        }
+        if(strtotime($day)==strtotime(date("Y-m-d", time()))){
+            $today='今天';
+        }
+        if(strtotime($day)==(strtotime(date("Y-m-d", time()))-3600*24)){
+            $yestoday='昨天';
+        }
+        
+        if(!empty($today)){
+            $table_type='now';
+        }
+        if(!empty($yestoday)){
+            $day =date('Y-m-d',strtotime('-1 day'));
+        }
+        $like[] = array("message.from_user_name", "not like", '%gh_%');
+        if($table_type == 'now'){
+            $like[] = array("message.created", ">=", date('Y-m-d 00:00:00', time()));
+            $like[] = array("message.created", "<=", date('Y-m-d 23:59:59', time()));
+        }else{
+            $like[] = array("message.created", ">=", date('Y-m-d 00:00:00', strtotime($day)));
+            $like[] = array("message.created", "<=", date('Y-m-d 23:59:59', strtotime($day)));
+        }
+        $list=Message::where($where)->where($like)->orderBy('message.id','desc')->paginate(20);
+//        $list = User::where($where)->where($like)->leftjoin("user_sns", "user_sns.openid", "=", "message.from_user_name")->leftjoin("users", "user_sns.user_id", "=", "users.id")->orderBy('message.id','desc')->select('message.*', 'users.nickname')->paginate(20);
+        $append = array();
+        $re = $request->toArray();
+        foreach ($re as $key => $value) {
+            $append[$key] = $value;
+        }
+        $list->appends($append);
+        return view('wechat::message.index',['list' => $list]);
+    }
+
+
+}
---
+++ resources/views/message/index.blade.php
@@ -0,0 +1,123 @@
+@extends(config("wxconfig.extends")) @section('content')
+    <section class="content-header">
+        <h1>
+            粉丝消息
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="/login"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">粉丝消息</li>
+        </ol>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">搜索</h3>
+                        <div class="box-tools"></div>
+                    </div>
+                    <div class="box-body">
+                        <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                    <form class="form-inline" role="form" method="get" id="frmSearch">
+                        <input type="submit" name="today" class="btn  btn-success" value="今天">&nbsp;
+                        <input type="submit" name="yesterday" class="btn  btn-success" value="昨天">&nbsp;
+                        <div class="input-group date">
+                            <div class="input-group-addon">
+                                <i class="fa fa-calendar"></i>
+                            </div>
+                            <input class="form-control pull-right" name="day" id="datepicker" type="text" placeholder="日期">
+                        </div>
+                        <div class="form-group">
+                            <select name="msg_type" class="form-control input-sm" style="width:7em">
+                                <option value="0">全部消息</option>
+                                <option value="text">文本</option>
+                                <option value="image">图片</option>
+                                <option value="voice">语音</option>
+                                <option value="video">视频</option>
+                                <option value="location">位置</option>
+                                <option value="link">链接</option>
+                                <option value="event">事件</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="content" class="form-control input-sm" style="width:9em" placeholder="消息内容">
+                        </div>&nbsp;
+                        <input type="submit" class="btn  btn-success" value="搜索">&nbsp;
+                        <a href="/message" class="btn  btn-primary">重置搜索条件</a>
+                    </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">用户</h3>
+                        <div class="box-tools">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                            <div class="row">
+                                <div class="col-sm-6"></div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <table id="example2" class="table table-bordered table-hover dataTable"
+                                           role="grid" aria-describedby="example2_info">
+                                        <tr role="row">
+                                            <th>用户</th>
+                                            <th>消息内容</th>
+                                            <th>消息类型</th>
+                                            <th>时间</th>
+                                        </tr>
+                                        @foreach($list as $val)
+                                            <tr>
+                                                <td>{{$val['openid']}}</td>
+                                                @if($val['msg_type'] == 'event')
+                                                <td>{{$val['event_key']}}</td>
+                                                @else
+                                                <td>{{$val['content']}}</td>
+                                                @endif
+                                                <td>{{$val['msg_type']}}</td>
+                                                <td>{{$val['created']}}</td>
+                                            </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                            @include("wechat::pagination.tfoot",['paginator'=>$list])
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="{{url("/bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js")}}"></script>
+    <script>
+        
+        $(function(){
+            $('#datepicker').datepicker({
+                format: 'yyyy-mm-dd',
+                autoclose: true
+            });
+            var search = location.search.substring(1);
+            if (search) {
+                try {
+                    var query = JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+
+                    $.each(query, function(k, v) {
+                        $('[name=' + k + ']').val(v);
+                    });
+                } catch (e) {
+                    console.log(e);
+                }
+
+            }
+        })
+    </script>
+@endsection
